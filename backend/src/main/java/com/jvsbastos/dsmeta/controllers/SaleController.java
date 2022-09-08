d944b1e224685649b011a7a2d4612b43c31e7193
joaovitorsb97 <jvs.bastos@gmail.com>
2022-09-08 01:15:57 -0300

    Date select and using Page instead of List

--- backend/src/main/java/com/jvsbastos/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/jvsbastos/dsmeta/controllers/SaleController.java
@@ -3,10 +3,13 @@ package com.jvsbastos.dsmeta.controllers;
 import com.jvsbastos.dsmeta.entities.Sale;
 import com.jvsbastos.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,7 +21,10 @@ public class SaleController {
     private SaleService saleService;
 
     @GetMapping
-    public ResponseEntity<List<Sale>> findAll(){
-        return ResponseEntity.status(HttpStatus.OK).body(saleService.findAll());
+    public ResponseEntity<Page<Sale>> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(saleService.findSales(minDate, maxDate, pageable));
     }
 }
